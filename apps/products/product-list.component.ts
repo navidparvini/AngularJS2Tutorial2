a45892982a492ca2039fabf5464bb273083c19df
Pyroj <pygithub@yahoo.com>
2017-03-10 00:30:56 +0330

    My second tutorial test codes

---
+++ apps/products/product-list.component.ts
@@ -0,0 +1,39 @@
+﻿import { Component } from 'angular2/core';
+
+@Component({
+    selector: 'pm-products',
+    templateUrl: 'apps/products/product-list.component.html'
+})
+export class ProductListComponent {
+    pageTitle: string = 'Product List';
+    imageWidth: number = 50;
+    imageMargin: number = 2;
+    showImage: boolean = false;
+    listFilter: string = 'cart';
+    products: any[] = [
+        {
+            "productId": 2,
+            "productName": "Garden Cart",
+            "productCode": "GDN-0023",
+            "releaseDate": "March 18, 2016",
+            "description": "15 gallon capacity rolling garden cart",
+            "price": 32.99,
+            "starRating": 4.2,
+            "imageUrl": "app/assets/images/garden_cart.png"
+        },
+        {
+            "productId": 5,
+            "productName": "Hammer",
+            "productCode": "TBX-0048",
+            "releaseDate": "May 21, 2016",
+            "description": "Curved claw steel hammer",
+            "price": 8.9,
+            "starRating": 4.8,
+            "imageUrl": "app/assets/images/rejon_Hammer.png"
+        }
+    ];
+
+    toggleImage(): void {
+        this.showImage = !this.showImage;
+    }
+}
